7fc565cb2e399d3b3339cff4f176184cb76d61d6
Karan <karandeepsingh47@gmail.com>
2019-09-26 13:19:45 -0700

    Season App using ReactJS - Fetch accurate location of User using geolocation API - Fetch the Current Date() - Displays and renders the right page depending upon these 2 parameters

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './SeasonDisplay.css'
+
+const seasonConfig = {
+    summer: {
+        text: 'Let\'s hit the beach',
+        iconName: 'sun'
+    },
+    winter: {
+        text: 'Burry it is cold!',
+        iconName: 'snowflake'
+    }
+}
+
+const getSeason = (lat, month) => {
+    if(month > 2 && month < 9) {
+        return lat > 0 ? 'summer' : 'winter';
+    } else {
+        return lat > 0 ? 'winter' : 'summer';
+    }
+}
+
+const SeasonDisplay = (props) => {
+    let lat = props.lat;
+
+    const season = getSeason(lat, new Date().getMonth());
+    const {text, iconName} = seasonConfig[season];
+
+    return (
+        <div className={`season-display ${season}`}>
+            <i className={`icon-left massive ${iconName} icon`}></i>
+            <h2>{text}</h2>
+            <i className={`icon-right massive ${iconName} icon`}></i>
+        </div>
+    );
+}
+
+export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component {
+    state = {
+        latitude: null,
+        errorMessage: null
+    };
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    componentDidMount() {
+        console.log('componentDidMount() called');
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({ latitude: position.coords.latitude }),
+            err => this.setState({ errorMessage: err.message })
+        );
+    }
+
+    componentDidUpdate() {
+        console.log('componentDidUpdate() called');
+    }
+
+    renderContent() {
+        let {latitude} = this.state,
+            {errorMessage} = this.state;
+
+        if(errorMessage && !latitude)
+            return (
+                <h4>Error: {errorMessage}</h4>
+            );
+
+        if(!errorMessage && latitude)
+            return (
+                <div>
+                    {/* <h4>Latitude: {latitude}</h4> */}
+                    <SeasonDisplay lat = {latitude} />
+                </div>
+            );
+
+        return (
+            <Spinner message = "Please accept location request..." />
+        );
+    }
+
+    render() {
+        console.log('render() called');
+
+        return(
+            <div className="border red">{this.renderContent()}</div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
